19f88cd3fba6da2f05eadccfd2a43659b1b7b550
Patterson <danielledionne@Dionnes-MacBook-Pro.local>
2020-08-14 15:56:34 -0400

    committing csv files

---
+++ PyBank/Resources/budget.py
@@ -0,0 +1,41 @@
+import os
+import csv
+
+budget_data.csv = os.path.join('..", "Resources", 'budget_data.csv')
+
+with open(budget_csv) as csv_file:
+    csv_reader = csv.reader(csv-file, delimiter=",")
+
+    csv_header = next(csv_file)
+    print(f"Header: {csv_header}")
+
+    for row in csv_reader:
+
+        date = str(budget_data[0])
+        profit/losses = int(budget_data[1])
+
+        ("date", "count")
+
+            total = sum("profit/losses")
+    
+            avgerage = sum("profit/losses")/len("date")
+
+            increase = "profit/losses"
+
+            decease = "profit/losses"
+
+    print("Financial Analysis")
+
+    print"----------------------------"
+
+    print ("Total Months", + [date])
+    print ("Total", [total] )
+    print ("Average Change", [average])
+    print ("Greates Increase in Profits", + str(increase + date)
+    print ("Greatest Decrease in Profits", + str(decrese + date)
+
+
+   def print_percentages(budget_data):
+
+
+    header = next(csvreader)
