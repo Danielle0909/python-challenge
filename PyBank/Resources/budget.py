c083d0b99b9250934ebe4008a732bbfb793411f5
Patterson <danielledionne@Dionnes-MacBook-Pro.local>
2020-08-16 00:35:17 -0400

    committing csv files

--- PyBank/Resources/budget.py
+++ PyBank/Resources/budget.py
@@ -1,41 +1,52 @@
 import os
 import csv
 
+
+date = []
+profit = []
+monthly_change = []
+
+total_profit = 0
+count = 0
+profitlosstotalchange = 0
+
+
 budget_data.csv = os.path.join('..", "Resources", 'budget_data.csv')
 
 with open(budget_csv) as csv_file:
-    csv_reader = csv.reader(csv-file, delimiter=",")
+    csv_reader = csv.reader(csv_file, delimiter=",")
 
     csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
+  
 
     for row in csv_reader:
 
-        date = str(budget_data[0])
-        profit/losses = int(budget_data[1])
+        date.append(row[0])
 
-        ("date", "count")
+        profit.append(row{1})
+        total_profit = total_profit + int(row[1])
 
-            total = sum("profit/losses")
+        total_profit = profitlosstotalchange
+        
+        monthlist = len(dates)
+       
+        total = sum("profit_loss_change")
+       
+        avgerage = sum("total_profit")/len("count")
+       
+        increase = "profit_loss_change" = max(monthly_change)
+       
+        decease = "profit_loss_change" = min(monthly_change)
+
     
-            avgerage = sum("profit/losses")/len("date")
-
-            increase = "profit/losses"
-
-            decease = "profit/losses"
-
     print("Financial Analysis")
 
     print"----------------------------"
 
-    print ("Total Months", + [date])
-    print ("Total", [total] )
-    print ("Average Change", [average])
+    print ("Total Months", + str[count])
+    print ("Total Profit", + str[total])
+    print ("Average Change", + [average])
     print ("Greates Increase in Profits", + str(increase + date)
     print ("Greatest Decrease in Profits", + str(decrese + date)
 
 
-   def print_percentages(budget_data):
-
-
-    header = next(csvreader)
